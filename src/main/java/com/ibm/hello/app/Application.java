00acb3764d9c110ab6a55a4377880f99d448a35f
Sean Sundberg <seansund@us.ibm.com>
2019-11-12 21:47:35 -0600

    Adds logic to Application to print swagger endpoint

--- src/main/java/com/ibm/hello/app/Application.java
+++ src/main/java/com/ibm/hello/app/Application.java
@@ -1,33 +1,33 @@
 package com.ibm.hello.app;
 
-import java.util.Arrays;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
-@ComponentScan({"com.ibm.hello.*", "com.ibm.cloud_garage.*","com.ibm.health"})
+@ComponentScan({"com.ibm.hello.*", "com.ibm.cloud_garage.*", "com.ibm.health"})
 public class Application extends SpringBootServletInitializer {
+    @Autowired
+    Environment environment;
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
-    //@Bean
+    @Bean
     public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
         return args -> {
+            String port = environment.getProperty("local.server.port");
 
-            System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-            String[] beanNames = ctx.getBeanDefinitionNames();
-            Arrays.sort(beanNames);
-            for (String beanName : beanNames) {
-                System.out.println(beanName);
-            }
+            System.out.println();
+            System.out.println("Server started - http://localhost:" + port + "/swagger-ui.html");
         };
     }
 
