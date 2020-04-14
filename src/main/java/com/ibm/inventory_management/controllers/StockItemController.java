0fc1213a2a497470d37768f7ac57f0452bb17092
Sean Sundberg <seansund@us.ibm.com>
2020-04-14 01:41:57 -0500

    Adds OpenTracing

--- src/main/java/com/ibm/inventory_management/controllers/StockItemController.java
+++ src/main/java/com/ibm/inventory_management/controllers/StockItemController.java
@@ -3,6 +3,8 @@ package com.ibm.inventory_management.controllers;
 import java.util.ArrayList;
 import java.util.List;
 
+import io.opentracing.Span;
+import io.opentracing.Tracer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,19 +15,28 @@ import com.ibm.inventory_management.services.StockItemApi;
 public class StockItemController {
 
     private final StockItemApi service;
+    private final Tracer tracer;
 
-    public StockItemController(StockItemApi service) {
+    public StockItemController(StockItemApi service, Tracer tracer) {
         this.service = service;
+        this.tracer = tracer;
     }
 
     @GetMapping(path = "/stock-items", produces = "application/json")
     public List<StockItem> listStockItems() {
+        Span span = this.tracer.buildSpan("stock-items").start();
+
         List<StockItem> items = new ArrayList<StockItem>();
         try {
             items = this.service.listStockItems();
+
+            span.log("Got items: " + items);
         } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            span.finish();
         }
+
         return items;
     }
 }
