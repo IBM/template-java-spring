42ff035be5c68c76826e62427d4084e06b8606ae
Sean Sundberg <seansund@us.ibm.com>
2020-04-14 10:31:15 -0500

    Removes unused imports

--- src/main/java/com/ibm/inventory_management/controllers/StockItemController.java
+++ src/main/java/com/ibm/inventory_management/controllers/StockItemController.java
@@ -1,17 +1,14 @@
 package com.ibm.inventory_management.controllers;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import io.opentracing.Span;
-import io.opentracing.Tracer;
+import com.ibm.inventory_management.models.StockItem;
+import com.ibm.inventory_management.services.StockItemApi;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ibm.inventory_management.models.StockItem;
-import com.ibm.inventory_management.services.StockItemApi;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class StockItemController {
